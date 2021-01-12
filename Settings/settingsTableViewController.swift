27d8b32effd1493ee98e92add80508f618747b25
Brandon Gonzalez <barelyabsolut@gmail.com>
2021-01-11 23:20:36 -0600

    New versión for 2021

--- Settings/settingsTableViewController.swift
+++ Settings/settingsTableViewController.swift
@@ -23,18 +23,27 @@ class settingsTableViewController: UITableViewController {
     
     @IBAction func logOut(_ sender: Any) {
         
-        UserDefaults.standard.set(false, forKey: "loggedIn")
+        let alert = UIAlertController(title: "¿Borrar todo?", message: "Esto borrará tu nombre, tus marcadores y otra información. ¿Continuar?", preferredStyle: .alert)
+
+        alert.addAction(UIAlertAction(title: "Cancelar", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Reiniciar", style: .default, handler: { action in
+                                   
+            UserDefaults.standard.set(false, forKey: "loggedIn")
+            
+            UserDefaults.standard.removePersistentDomain(forName: Bundle.main.bundleIdentifier!)
+            UserDefaults.standard.synchronize()
+            
+            guard let rootVC = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "loginViewController") as? loginViewController else {
+                return
+            }
+            
+            UIApplication.shared.windows.first?.rootViewController = rootVC
+            UIApplication.shared.windows.first?.makeKeyAndVisible()
+            
+        }))
         
-        UserDefaults.standard.removePersistentDomain(forName: Bundle.main.bundleIdentifier!)
-        UserDefaults.standard.synchronize()
-        
-        guard let rootVC = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "loginViewController") as? loginViewController else {
-            return
-        }
-        
-        UIApplication.shared.windows.first?.rootViewController = rootVC
-        UIApplication.shared.windows.first?.makeKeyAndVisible()
-        
+        self.present(alert, animated: true)
+
     }
     
 }
